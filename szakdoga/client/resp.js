b4892ef92382a627c0657fffdfbc06055a249ff3
Toma08 <91669816+Toma08@users.noreply.github.com>
2022-05-02 09:01:01 +0200

    Delete szakdoga directory

--- szakdoga/client/resp.js
+++
@@ -1,6 +0,0 @@
-const tglbutton=document.getElementsByClassName('toggl-buttons')[0]
-const navlinks=document.getElementsByClassName('navba-links')[0]
-
-tglbutton.addEventListener('click',()=>{
-    navlinks.classList.toggle('active')
-})
